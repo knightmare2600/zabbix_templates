ad24e12869bf38939495fc9ba97cb79f872dfc0e
knightmare <knightmare@puppet.knight139.co.uk>
2016-11-25 14:31:14 +0000

    Update vmware snapshot to support consolidation checking too (knightmare2600)

--- vmware_snapshot_check/snapshot_check.sh
+++ vmware_snapshot_check/snapshot_check.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
 ##----------------------------------------------------------------------------##
-## Zabbix check script for esx to check existence of snapshot and where are.  ##
+## Zabbix check script for esx to check existence of snapshots, consolidation ##
+## required and to which VMs the status is applied. Outputs two files which   ##
+## are read by zabbix agent to monitor status.                                ##
+##                                                                            ##
+## Requirements:                                                              ##
+##  * /root/.ssh/id_rsa SSH key to permit passwordless login to ESXi          ##
+##  * Corresponding public key added to /etc/ssh/keys-root/authorized_keys    ##
+##  * /usr/lib/zabbix/externalscripts/snapshot_check.sh to exist              ##
+##  * Cron job to exist in /etc/crontab An example of the cron job would be:  ##
+##  /usr/lib/zabbix/externalscripts/snapshot_check.sh esx.your.tld 0 1 2>&1   ##
+##  * Zabbix 2.2+                                                             ##
+##                                                                            ##
 ## Script tested on vmware esx/esxi: 3.5/4.0/4.1/5.5/6.0                      ##
 ##                                                                            ##
 ## on early version you have to create an alias for vim-cmd                   ##
@@ -15,6 +26,7 @@
 ## Updated: 17 Nov 2015   Robert McLay      Zabbix regex works out # of snaps ##
 ## Updated: 18 Nov 2015   Robert McLay      Confirm SSH works, alert if not   ##
 ## Updated: 19 Nov 2015   Duncan Blair      Only remove file if it exists     ##
+## Updated: 25 Nov 2016   Robert McLay      Check for consolidation too       ##
 ##----------------------------------------------------------------------------##
 
 print_usage() {
@@ -38,12 +50,30 @@ if [ "$#" -ne "3" ]; then
   print_usage
 fi
 
+## Pre-flight checks. Delete any previous files and clear vaiables
+
+## Check to see if the snapshot-status file exists
+if [ -f "/usr/lib/zabbix/externalscripts/snapshot-status" ]; then
+  ## Delete file if it does exist, to make sure we don't get wonky output
+  rm /usr/lib/zabbix/externalscripts/snapshot-status
+fi
+
+## check if consolidate-status file exists
+if [ -f "/usr/lib/zabbix/externalscripts/consolidation-status" ]; then
+  ## Delete file if it does exist, to make sure we don't get wonky output
+  rm /usr/lib/zabbix/externalscripts/consolidation-status
+fi
+
+## Reset variables
 i=0
 total=0
+snapshotnum=0
+
 
 ## Test we can SSH to the host before trying to execute anything
 testconnection=`ssh -i /root/.ssh/id_rsa root@$1 vmware -v`
 
+## Confirm SSH connection works, if not flub based on error code
 if [ "$?" -eq "130" ]; then 
   echo "SSH Connection Failed: Please check manually" > /usr/lib/zabbix/externalscripts/snapshot-status
   exit 0
@@ -52,43 +82,57 @@ elif [ "$?" -eq "255" ]; then
   exit 0
 fi
 
-echo "$?"
+## Debug code: will return error code of SSH uncomment to confirm. Expected result: 0 i.e. success
+#echo "$?"
 
 vms=(`ssh -i /root/.ssh/id_rsa root@$1 vim-cmd vmsvc/getallvms | sed -e '1d' -e 's/ \[.*$//' | awk '$1 ~ /^[0-9]+$/ {print  $1":"substr($0,8,80)}'|sort`);
 for vm in "${vms[@]}"; do
 id=`echo $vm |awk -F: '{print $1}'`
 vmname=`echo $vm |awk -F: '{print $2}'`
-result=`ssh -i /root/.ssh/id_rsa root@$1 vim-cmd vmsvc/snapshot.get $id |grep "Snapshot Name" |wc -l`
-  if [ $result -ge 1 ]; then
-    snap[$i]="$vmname:$result";
-    let "total=$total+$result";
+snapshotnum=`ssh -i /root/.ssh/id_rsa root@$1 vim-cmd vmsvc/snapshot.get $id |grep "Snapshot Name" |wc -l`
+consolidate=`ssh -i /root/.ssh/id_rsa root@$1 vim-cmd vmsvc/get.summary $id |grep "consolidationNeeded" | awk '{ print $3 }'| tr -d ','`
+
+## Debug code, uncomment to confirm check works as one expects. Should (+20p) return true|false
+#echo "$consolidate"
+
+## Now check for snapshots and consolidation. I'll use a different file to keep Zabbix logic code
+## down consolidation is a bad thing (TM) so if it's happening, then y'all are getting an alert
+  if [ $snapshotnum -ge 1 ]; then
+    snap[$i]="$vmname:$snapshotnum";
+    let "snaptotal=$snaptotal+$snapshotnum";
     let i++;
+  fi
+  if [ "$consolidate" == "true" ]; then
+    ## Debug code, uncomment to confirm logic code works
+    #echo "Consolidation status of $vmname is $consolidate"
+    echo "$vmname: Needs Consolidation" >> /usr/lib/zabbix/externalscripts/consolidation-status
   fi
 done
 
-## Check to see if the snapshot-status file exists
-if [ -f "/usr/lib/zabbix/externalscripts/snapshot-status" ]; then
-  ## Delete file if it does exist, to make sure we don't get wonky output
-  rm /usr/lib/zabbix/externalscripts/snapshot-status
+## If /usr/lib/zabbix/externalscripts/consolidation-status does not exist, then no consolidation required
+if [ ! -f "/usr/lib/zabbix/externalscripts/consolidation-status" ]; then
+  echo "No consolidation required" > /usr/lib/zabbix/externalscripts/consolidation-status
 fi
 
+
 ## If total of snapshots is less than the tolerance value (2nd variable) then we're all square
-if [ $total -le $2 ] ; then
-  if [ $total -eq 0 ] ; then
-    echo "$total snapshots found" > /usr/lib/zabbix/externalscripts/snapshot-status
+if [ $snapshotnum -le $2 ]; then
+  if [ $snapshotnum -eq 0 ]; then
+    echo "$snapshotnum snapshots found" > /usr/lib/zabbix/externalscripts/snapshot-status
     exit 0
   else
-    echo "$total snapshots found on ${snap[@]}"  > /usr/lib/zabbix/externalscripts/snapshot-status
+    echo "$snapshotnum snapshots found on ${snap[@]}"  > /usr/lib/zabbix/externalscripts/snapshot-status
     exit 0
   fi
 fi
 
 ## If total of snapshots is less than the critical level, output that, otherwise lp0 is combusting
-if [ $total -le $3 ] ; then
-  echo "$total snapshots found on ${snap[@]}" > /usr/lib/zabbix/externalscripts/snapshot-status
+if [ $snapshotnum -le $3 ]; then
+  echo "$snapshotnum snapshots found on ${snap[@]}" > /usr/lib/zabbix/externalscripts/snapshot-status
   exit 1
 else
-  echo "$total snapshots found on ${snap[@]}" > /usr/lib/zabbix/externalscripts/snapshot-status
+  echo "$snapshotnum snapshots found on ${snap[@]}" > /usr/lib/zabbix/externalscripts/snapshot-status
   exit 2
 fi
 
+exit 0
